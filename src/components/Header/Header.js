c9c49c1e18a571b7322e6dacd5f5b0b93721b5eb
Andrzej Nawalaniec <46224560+baqardo@users.noreply.github.com>
2021-05-01 13:04:40 +0200

    Header element

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,48 @@ import React from "react";
 
 import "./Header.scss";
 
-const Header = props => <header className="header"></header>;
+import scrollIcon from "../../assets/svg/scroll.svg";
+
+const Header = props => (
+  <header className="header">
+    <div className="header__cta">
+      <div className="header__cta-title">
+        <p className="header__cta-line">Explore the world</p>
+        <p className="header__cta-line header__cta-line--pushed">with exciting people!</p>
+      </div>
+      <div className="header__cta-mission">We help people to find ideal company for the joint trips</div>
+      <a className="header__cta-btn" href="#home">
+        Start
+      </a>
+    </div>
+
+    <div className="header__steps">
+      <div className="header__steps-item">
+        <p className="header__steps-number">01</p>
+        <p className="header__steps-title">Select Destination</p>
+        <p className="header__steps-description">Search and find a destination to travel and refresh!</p>
+      </div>
+      <div className="header__steps-item">
+        <p className="header__steps-number">02</p>
+        <p className="header__steps-title">Find People</p>
+        <p className="header__steps-description">
+          Find the people who are travelling to the destination and connect with them
+        </p>
+      </div>
+      <div className="header__steps-item">
+        <p className="header__steps-number">03</p>
+        <p className="header__steps-title">Explore</p>
+        <p className="header__steps-description">Experience the Journey with like-minded people!</p>
+      </div>
+    </div>
+
+    <div className="header__more">
+      <p className="header__more-text">Scroll to see more</p>
+      <div className="header__more-scroll">
+        <img className="header__more-arrow" src={scrollIcon} alt="arrow  down" />
+      </div>
+    </div>
+  </header>
+);
 
 export default Header;
